77b6fa31853c1e08d3ef5675c1126ed0efe809cc
Pawel Rybialek <pawel.rybialek@gmail.com>
2019-05-18 14:05:09 +0200

    calculate final mark

--- src/sample/magic/constants/Constants.java
+++ src/sample/magic/constants/Constants.java
@@ -1,7 +1,7 @@
 package sample.magic.constants;
 
 public interface Constants {
-    String WINDOW_TITLE = "Validator ~by Szpaczyński";
+    String WINDOW_TITLE = "Check mark DMC";
 
     String HEADER_TEXT = "Provide code";
     String HEADER_FONT = "Tahoma";
@@ -13,6 +13,7 @@ public interface Constants {
     String CLEAR_BUTTON_DESC = "Clear";
 
     int GAP_SIZE = 10;
+    int DIVISOR = 43;
     int HEADER_SIZE = 20;
     int PADDING_SIZE = 25;
     int WINDOW_WIDTH = 300;
--- src/sample/magic/converter/CodeConverter.java
+++ src/sample/magic/converter/CodeConverter.java
@@ -1,9 +1,92 @@
 package sample.magic.converter;
 
-public class CodeConverter {
+import sample.magic.constants.Constants;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class CodeConverter implements Constants {
 
     public static String convert(String code) {
-        return code;
+        Map<Character, Integer> norm = generateNorm();
+
+        int sum = 0;
+        for (Character c : code.toCharArray()) {
+            if (norm.containsKey(c)) {
+                sum += norm.get(c);
+            } else {
+                throw new NotImplementedException();
+            }
+        }
+
+        int resultNumber = sum % DIVISOR;
+
+        return code + matchResult(norm, resultNumber);
+    }
+
+    private static Character matchResult(Map<Character, Integer> norm, int resultNumber) {
+
+        for(Map.Entry<Character, Integer> entry : norm.entrySet()) {
+            if(Objects.equals(resultNumber, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+
+        return null;
+    }
+
+    private static Map<Character, Integer> generateNorm() {
+        return new HashMap<Character, Integer>() {{
+            put('0', 0);
+            put('1', 1);
+            put('2', 2);
+            put('3', 3);
+            put('4', 4);
+            put('5', 5);
+            put('6', 6);
+            put('7', 7);
+            put('8', 8);
+            put('9', 9);
+            put('A', 10);
+            put('B', 11);
+            put('C', 12);
+            put('D', 13);
+            put('E', 14);
+            put('F', 15);
+            put('G', 16);
+            put('H', 17);
+            put('I', 18);
+            put('J', 19);
+            put('K', 20);
+            put('L', 21);
+            put('M', 22);
+            put('N', 23);
+            put('O', 24);
+            put('P', 25);
+            put('Q', 26);
+            put('R', 27);
+            put('S', 28);
+            put('T', 29);
+            put('U', 30);
+            put('V', 31);
+            put('W', 32);
+            put('X', 33);
+            put('Y', 34);
+            put('Z', 35);
+            put('-', 36);
+            put('.', 37);
+            put(' ', 38);
+            put('$', 39);
+            put('/', 40);
+            put('+', 41);
+            put('%', 42);
+            put('#', null);
+            put('&', null);
+            put('*', null);
+            put('=', null);
+        }};
     }
 
 }
